ca7a8d9ab0515ab90d7c23e6c218282d98c40dc1
AAydogan35 <alperen.aydogan@boun.edu.tr>
2022-04-23 13:43:53 +0300

    implemented signup functions and connected buttons to navigate

--- WhiteBoardProject/settings.py
+++ WhiteBoardProject/settings.py
@@ -138,3 +138,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+ACCOUNT_EMAIL_REQUIRED = True
+
+# For testing signups
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
