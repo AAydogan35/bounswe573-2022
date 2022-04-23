940472ea2d8720cd1bd7984f6299028d44097ace
AAydogan35 <alperen.aydogan@boun.edu.tr>
2022-04-23 22:45:30 +0300

    Added share button and added body to share box class PostForm

--- cospace/forms.py
+++ cospace/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    body = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'rows': '3',
+            'placeholder': 'Add content'
+        })
+    )
 
     class Meta:
         model = Post
