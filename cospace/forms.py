e7991e124a57484ca28cd471b82cf1b7995888f2
AAydogan35 <alperen.aydogan@boun.edu.tr>
2022-05-03 17:30:50 +0300

    Added comment section to post details

--- cospace/forms.py
+++ cospace/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 from tinymce.widgets import TinyMCE
 
 class PostForm(forms.ModelForm):
@@ -14,3 +14,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['body']
+
+
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(
+        label='',
+        widget=TinyMCE(attrs={
+            'rows': '3',
+            'placeholder': 'Add content'
+        })
+    )
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
--- cospace/views.py
+++ cospace/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 class PostListView(View):
@@ -35,9 +35,10 @@ class PostListView(View):
 class PostDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
-
+        form = CommentForm()
         context = {
-            'post': post
+            'post': post,
+            'form': form,
 
         }
 
